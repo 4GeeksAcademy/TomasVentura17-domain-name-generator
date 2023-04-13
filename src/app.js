425616078f91428d9fc45c8ac0c18f17a0d0a3db
TomasVentura17 <126104459+TomasVentura17@users.noreply.github.com>
2023-04-13 01:43:51 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the", "our", "what"];
+var adj = ["great", "big", "giant"];
+var noun = ["jogger", "racoon", "dog"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
+}
